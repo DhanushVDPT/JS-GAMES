38eadf55222643c9cab1c2ac20ae8f51a433dda9
Dhanush <be19b034@smail.iitm.ac.in>
2021-08-17 11:40:40 +0530

    images

--- app.js
+++ app.js
@@ -3,83 +3,117 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardArray =[
     {
         name: 'fries',
-        img: '.images/fries.png'
+        img: './images/fries.jpg'
     },
     {
         name: 'fries',
-        img: '.images/fries.png'
+        img: './images/fries.jpg'
     },
     {
         name: 'cheeseburger',
-        img: '.images/cheeseburger.png'
+        img: './images/cheeseburger.jpg'
     },
     {
         name: 'cheeseburger',
-        img: '.images/cheeseburger.png'
+        img: './images/cheeseburger.jpg'
     },
     {
         name: 'hotdog',
-        img: '.images/hotdog.png'
+        img: './images/hotdog.jpg'
     },
     {
         name: 'hotdog',
-        img: '.images/hotdog.png'
+        img: './images/hotdog.jpg'
     },
     {
         name: 'ice-cream',
-        img: '.images/ice-cream.png'
+        img: './images/ice-cream.jpg'
     },
     {
         name: 'ice-cream',
-        img: '.images/ice-cream.png'
+        img: './images/ice-cream.jpg'
     },
     {
         name: 'milkshake',
-        img: '.images/milkshake.png'
+        img: './images/milkshake.jpg'
     },
     {
         name: 'ice-cream',
-        img: '.images/milkshake.png'
+        img: './images/milkshake.jpg'
     },
     {
         name: 'pizza',
-        img: '.images/pizza.png'
+        img: './images/pizza.jpg'
     },
     {
         name: 'pizza',
-        img: '.images/pizza.png'
+        img: './images/pizza.jpg'
     }
-]})
+]
 
-const grid = document.querySelector('.grid');
-var cardsChosen = [];
-var cardsChosenId = [];
+    cardArray.sort(() => 0.5 - Math.random()); //this will reload the game by randomly sorting the cards
 
-//create board
-function createBoard() {
-    for (let i=0; i<cardArray.length; i++) {
-        var card = document.createElement('img');
-        card.setAttribute('src', '.images/blank.png');
-        card.setAttribute('data-id', i);
-        // card.addEventListener('click', flipcard)
-        grid.appendChild(card);
+    const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#result'); //will be used to display result on webpage
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    var cardsWon = [];
+
+    //create board
+    function createBoard() {
+        for (let i=0; i<cardArray.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src', './images/blank.jpg');
+            card.setAttribute('data-id', i);
+            // card.addEventListener('click', flipcard)
+            grid.appendChild(card);
+        }
     }
-}
 
-//check matches
+    //check matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img') //gets all the images
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+        if(cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match');
+            cards[optionOneId].setAttribute('src', './images/white.png')
+            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            //this block checked if the names of the chosen cards matched and replaced the matched cards with white blank image if they matched
+            cardsWon.push(cardsChosen);
+            //the cards matched are pushed to cardswon array
+        }
+        else{
+            //if the cards don't match, they should be flipped back to play again
+            cards[optionOneId].setAttribute('src', './gitimages/blank.jpg');
+            alert('Sorry, try again');
+        }
+        //even if cards match or don't match, the chosen cards array and chosen cards id array must be cleared for playing again
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+        if(cardsWon.length === cardArray.length/2){
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+            //this block checks if all the cards are found
+        }
+    }
 
-//flip card
-function flipcard() {
-    var cardId = this.getAttribute('data-id');
-    cardsChosen.push(cardArray[cardId].name);
-    cardsChosenId.push(cardId);
-    this.setAttribute('src', cardArray);
-    
-}
+    //flip card
+    function flipcard() {
+        var cardId = this.getAttribute('data-id'); //this will get the ID number of card which was clicked
+        cardsChosen.push(cardArray[cardId].name); //this will push the clicked card's name into the array of selected cards
+        cardsChosenId.push(cardId); //this will push the id of clicked card into selected card's id array
+        this.setAttribute('src', cardArray); //the clicked card is being added the image it should display by flipping
+        //now we don't want more than two cards to be selected at a time, so let's do that:
+        if(cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500) //set timeout will give buffer time. match is checked after 500ms
+        }
+        
+    }
 
-createBoard()
+    createBoard();
 
-
+})
 
 
 
